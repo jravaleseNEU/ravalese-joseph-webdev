4db5882b4abe592894acd7b2054aca1f4aa60f3d
ravalese <ravalese.j@husky.neu.edu>
2017-07-20 19:13:16 -0400

    assignment 3.2 widget.service.client

--- public/assignment/widget.service.client.js
+++ public/assignment/widget.service.client.js
@@ -1,14 +1,20 @@
 (function () {
     angular
         .module("WamApp")
-        .factory("WidgetService", WidgetService);
+        .factory("widgetService", widgetService);
 
-    function WidgetService() {
+    function widgetService() {
 
-        var Widgets = [
-            { "_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem" },
-            { "_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem" },
-            { "_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem" }
+        var widgets = [
+            { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
+            { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+                "url": "http://lorempixel.com/400/200/"},
+            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+            { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+                "url": "https://youtu.be/AM2Ivdi9c4E" },
+            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
         ];
 
         var api = {
@@ -20,35 +26,35 @@
         };
         return api;
 
-        function updateWidget(websiteId, Widget) {
-            for(var u in Widgets) {
-                if(Widgets[u].websiteId === websiteId) {
-                    Widget[u] = Widget;
+        function updateWidget(widgetId, widget) {
+            for(var u in widgets) {
+                if(widgets[u]._id === widgetId) {
+                    widget[u] = widget;
                     return;
                 }
             }
             return null;
         }
 //WORK ON THIS
-        function createWidget(websiteId, Widget) {
-            Widget._id = (new Date()).getTime() + "";
-            Widget.websiteId = websiteId;
-            Widgets.push(Widget);
-            return Widget;
+        function createWidget(pageId, widget) {
+            widget._id = (new Date()).getTime() + "";
+            widget.pageId = pageId;
+            widgets.push(widget);
+            return widget;
         }
 
-        function findWidgetByWebsiteId(websiteId) {
-            for(var u in Widgets) {
-                if(Widgets[u].websiteId === websiteId) {
-                    return Widgets[u];
+        function findWidgetByPageId(pageId) {
+            for(var u in widgets) {
+                if(widgets[u].pageId === pageId) {
+                    return widgets[u];
                 }
             }
             return null;
         }
-        function findWidgetById(WidgetId) {
-            for(var u in Widgets) {
-                if(Widgets[u]._id === WidgetId {
-                    return Widgets[u];
+        function findWidgetById(widgetId) {
+            for(var u in widgets) {
+                if(widgets[u]._id === widgetId {
+                    return widgets[u];
                 }
             }
             return null;
@@ -56,11 +62,11 @@
 
 
 //WORK ON THIS
-        function deleteWidget(WidgetId) {
-            for(var u in Widgets) {
-                var _Widget = Widget[u];
-                if(_Widget._id === websiteId) {
-                    Widgets.pop(Widget);
+        function deleteWidget(widgetId) {
+            for(var u in widgets) {
+                var _widget = widget[u];
+                if(_widget._id === widgetId) {
+                    widgets.pop(widget);
                 }
             }
             return null;
