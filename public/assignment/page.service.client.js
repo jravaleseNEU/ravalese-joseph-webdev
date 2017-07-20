79027a8c1cb76ba2fb13493e89029e60205c45e9
ravalese <ravalese.j@husky.neu.edu>
2017-07-20 19:07:00 -0400

    assignment 3.2 page.service.client

--- public/assignment/page.service.client.js
+++ public/assignment/page.service.client.js
@@ -1,3 +1,70 @@
-/**
- * Created by ravalese on 7/20/17.
- */
+(function () {
+    angular
+        .module("WamApp")
+        .factory("pageService", pageService);
+
+    function pageService() {
+
+        var pages = [
+            { "_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem" },
+            { "_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem" },
+            { "_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem" }
+        ];
+
+        var api = {
+            "findPageByWebsiteId": findPageByWebsiteId,
+            "findPageById": findPageById,
+            "updatePage": updatePage,
+            "deletePage": deletePage,
+            "createPage": createPage
+        };
+        return api;
+
+        function updatePage(websiteId, page) {
+            for(var u in pages) {
+                if(pages[u].websiteId === websiteId) {
+                    page[u] = page;
+                    return;
+                }
+            }
+            return null;
+        }
+//WORK ON THIS
+        function createPage(websiteId, page) {
+            page._id = (new Date()).getTime() + "";
+            page.websiteId = websiteId;
+            pages.push(page);
+            return page;
+        }
+
+        function findPageByWebsiteId(websiteId) {
+            for(var u in pages) {
+                if(pages[u].websiteId === websiteId) {
+                    return pages[u];
+                }
+            }
+            return null;
+        }
+        function findPageById(pageId) {
+            for(var u in pages) {
+                if(pages[u]._id === pageId {
+                    return pages[u];
+                }
+            }
+            return null;
+        }
+
+
+//WORK ON THIS
+        function deletePage(pageId) {
+            for(var u in pages) {
+                var _page = page[u];
+                if(_page._id === websiteId) {
+                    pages.pop(page);
+                }
+            }
+            return null;
+        }
+
+    }
+})();
--- public/assignment/widget.service.client.js
+++ public/assignment/widget.service.client.js
@@ -1,3 +1,70 @@
-/**
- * Created by ravalese on 7/20/17.
- */
+(function () {
+    angular
+        .module("WamApp")
+        .factory("WidgetService", WidgetService);
+
+    function WidgetService() {
+
+        var Widgets = [
+            { "_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem" },
+            { "_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem" },
+            { "_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem" }
+        ];
+
+        var api = {
+            "findWidgetByWebsiteId": findWidgetByWebsiteId,
+            "findWidgetById": findWidgetById,
+            "updateWidget": updateWidget,
+            "deleteWidget": deleteWidget,
+            "createWidget": createWidget
+        };
+        return api;
+
+        function updateWidget(websiteId, Widget) {
+            for(var u in Widgets) {
+                if(Widgets[u].websiteId === websiteId) {
+                    Widget[u] = Widget;
+                    return;
+                }
+            }
+            return null;
+        }
+//WORK ON THIS
+        function createWidget(websiteId, Widget) {
+            Widget._id = (new Date()).getTime() + "";
+            Widget.websiteId = websiteId;
+            Widgets.push(Widget);
+            return Widget;
+        }
+
+        function findWidgetByWebsiteId(websiteId) {
+            for(var u in Widgets) {
+                if(Widgets[u].websiteId === websiteId) {
+                    return Widgets[u];
+                }
+            }
+            return null;
+        }
+        function findWidgetById(WidgetId) {
+            for(var u in Widgets) {
+                if(Widgets[u]._id === WidgetId {
+                    return Widgets[u];
+                }
+            }
+            return null;
+        }
+
+
+//WORK ON THIS
+        function deleteWidget(WidgetId) {
+            for(var u in Widgets) {
+                var _Widget = Widget[u];
+                if(_Widget._id === websiteId) {
+                    Widgets.pop(Widget);
+                }
+            }
+            return null;
+        }
+
+    }
+})();
