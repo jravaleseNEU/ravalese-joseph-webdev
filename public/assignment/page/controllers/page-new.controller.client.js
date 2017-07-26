298b39d3df0371b418ebacc7248f7deb3ae2368f
ravalese <ravalese.j@husky.neu.edu>
2017-07-25 21:19:49 -0400

    assignment 3.12 Page new page working

--- public/assignment/page/controllers/page-new.controller.client.js
+++ public/assignment/page/controllers/page-new.controller.client.js
@@ -8,12 +8,24 @@
 
         model.userId = $routeParams.userId;
         model.websiteId = $routeParams.wid;
+        model.pageId = $routeParams.pid;
 
 
 
         function init() {
             model.pages = pageService.findPagesByWebsiteId(model.websiteId);
+            model.page = pageService.findPageById(model.pageId);
         }
         init();
+
+        function createPage(websiteId,page) {
+            var _page = pageService.findPageById(model.pageId);
+            if(!_page) {
+                var page = pageService.createPage(websiteId,model.page);
+                $location.url("/user/:userId/website/:websiteId/page/" + page._id);
+            } else {
+                model.error = "page already exists";
+            }
+        }
     }
 })();
