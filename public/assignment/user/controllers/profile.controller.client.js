47555486163761c53bd15d2a5c08573854a58b4d
ravalese <ravalese.j@husky.neu.edu>
2017-07-26 00:11:10 -0400

    assignment 3.15 Website Pages routing correct

--- public/assignment/user/controllers/profile.controller.client.js
+++ public/assignment/user/controllers/profile.controller.client.js
@@ -6,13 +6,13 @@
 
     function profileController($routeParams, userService) {
         var model = this;
-        var userId = $routeParams["userId"];
+        model.userId = $routeParams.userId;
 
         model.updateUser = updateUser;
         model.unregister = unregister;
 
         function init() {
-            model.user = userService.findUserById(userId);
+            model.user = userService.findUserById(model.userId);
         }
         init();
 
